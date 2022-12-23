bc950600dfa7cc169c0b1f05084735a885480db5
Victor Magalhaes <victor.barreto@buser.com.br>
2022-12-23 20:30:04 -0300

    deploy

--- life-vue/vue.config.js
+++ life-vue/vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
 })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/VueJS version/'
+    : '/'
+}
